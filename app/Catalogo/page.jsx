fb077a38c05e4d979597f21894e99e8914373265
dudaribw <mr306573@gmail.com>
2024-09-24 10:49:55 -0300

    catalogo

--- app/Catalogo/page.jsx
+++ app/Catalogo/page.jsx
@@ -2,57 +2,117 @@
 import React, { useState } from 'react'
 import Styles from '@/app/Catalogo/catalogo.module.css'
 
-const Login = (props) => {
-    const [codigo, setEmail] = useState('')
-    const [autor, setPassword] = useState('')
-    const [titulo, setEmailError] = useState('')
-    const [assunto, setPasswordError] = useState('')
-    const [numero, setPasswordError] = useState('')
-    const [acervo, setPasswordError] = useState('')
-    const [ISBN, setPasswordError] = useState('')
-    const [quantidade, setPasswordError] = useState('')
 
-    return (
-        <>
-            <div className={Styles.formulario}></div>
-            <form>
-                <label className=''>
-                    Código do exemplar:
-                    <input type="text" name="codigo" />
-                </label>
-                <label>
-                    Nome do autor:
-                    <input type="text" name="autor" />
-                </label>
-                <label>
-                    Título do exemplar:
-                    <input type="text" name="titulo" />
-                </label>
-                <label>
-                    Código do assunto:
-                    <input type="text" name="assunto" />
-                </label>
-                <label>
-                    Número de chamada:
-                    <input type="number" name="numero" />
-                </label>
-                <label>
-                    Acervo:
-                    <input type="text" name="acervo" />
-                </label>
-                <label>
-                    ISBN:
-                    <input type="text" name="ISBN" />
-                </label>
-                <label>
-                    Quantidade:
-                    <input type="number" name="quantidade" />
-                </label>
-                <div className={Styles.inputContainer}>
-                    <input className={Styles.inputButton} type="button" onClick={onButtonClick} value={'Registrar'} />
-                </div>
-            </form>
+
+const Catalogo = (props) => {
+  const [codigo, setCodigo] = useState('')
+  const [autor, setAutor] = useState('')
+  const [titulo, setTitulo] = useState('')
+  const [assunto, setAssunto] = useState('')
+  const [numero, setNumero] = useState('')
+  const [acervo, setAcervo] = useState('')
+  const [ISBN, setISBN] = useState('')
+  const [quantidade, setQuantidade] = useState('')
+
+
+  const onButtonClick = () => {
+    // You'll update this function later...
+  }
+
+
+  return (
+    <>
+      <div className={Styles.inputContainer}>
+        <p>
+            Código do exemplar:  
+        </p>
+        <input
+          value={codigo}
+          onChange={(ev) => setCodigo(ev.target.value)}
+          className={Styles.inputBox} required/>
+      </div>
+      <div className={Styles.inputContainer}>
+        <p>
+            Nome do autor:
+        </p>
+        <input
+          value={autor}
+          onChange={(ev) => setAutor(ev.target.value)}
+          className={Styles.inputBox} required>
             
-        </>
-    )
+          </input>
+      </div>
+      <div className={Styles.inputContainer}>
+        <p>
+            Título do exemplar:
+        </p>
+        <input
+          value={titulo}
+          onChange={(ev) => setTitulo(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+      </div>
+      <div className={Styles.inputContainer}>
+        <p>
+            Código do assunto:
+        </p>
+        <input
+          value={assunto}
+          onChange={(ev) => setAssunto(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+      </div>
+      
+      <div className={Styles.inputContainer}>
+        <p>
+            Número de chamada:
+        </p>
+        <input
+          value={numero}
+          onChange={(ev) => setNumero(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+          </div>
+
+      <div className={Styles.inputContainer}>
+        <p>
+            Acervo:
+        </p>
+        <input
+          value={acervo}
+          onChange={(ev) => setAcervo(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+          </div>
+          
+      <div className={Styles.inputContainer}>
+        <p>
+            ISBN:
+        </p>
+        <input
+          value={ISBN}
+          onChange={(ev) => setISBN(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+          </div>
+          
+      <div className={Styles.inputContainer}>
+        <p>
+            Quantidade:
+        </p>
+        <input
+          value={quantidade}
+          onChange={(ev) => setQuantidade(ev.target.value)}
+          className={Styles.inputBox} required>
+          </input>
+          </div>
+
+      <div className={Styles.inputContainer}>
+        <input className={Styles.inputButton} type="button" onClick={onButtonClick} value={'Registrar'} />
+        </div>
+      
+      </>
+  )
 }
+
+export default Catalogo
