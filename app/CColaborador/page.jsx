26cb8c1e07aeb63fcb9c178bcf85d4cd3fa16225
dudaribw <mr306573@gmail.com>
2024-09-24 16:29:03 -0300

    Colaborador

--- app/CColaborador/page.jsx
+++ app/CColaborador/page.jsx
@@ -8,16 +8,36 @@ const CADASTRO = () => {
     const [telefone, setTelefone] = useState("");
     const [Dnasc, setDnasc] = useState("");
     const [CPF, setCPF] = useState("");
+    const [dados, setDados] = useState("");
+    const [senha, setSenha] = useState("");
 
+    const onButtonClick = () => {
+        // You'll update this function later...
+    }
 
     return (
         <>
+            
+            <input type="text"
+                value={dados}
+                placeholder="Dados pessoais"
+                onChange={(e) => setDados(e.target.value)} require
+                className={Style.inputBox} />
+
+            <input type="password"
+                value={senha}
+                placeholder="Senha"
+                onChange={(e) => setSenha(e.target.value)} require 
+                className={Style.inputType}/>
+
+            <br></br>
 
             <p className={Style.title} >
                 <label>Cadastro de Colaborador</label>
             </p>
 
             <form className={Style.form}>
+                <br></br>
                 <label>Nome Completo:</label>
                 <input type="text"
                     value={name}
@@ -50,30 +70,35 @@ const CADASTRO = () => {
                 <input type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)} required />
-                    <br></br>
+                <br></br>
 
-                    <form className={Style.email}>
+                <form className={Style.email}>
 
-                    <input type="checkbox" id="e-mail" name="e-mail" value="E-mail"/><label for="e-mail">Receber e-mail </label> 
+                    <input type="checkbox" id="e-mail" name="e-mail" value="E-mail" /><label for="e-mail">Receber e-mail </label>
 
                 </form>
 
                 <br></br>
 
-                    <label>Tipo de empréstimo:</label>
-                    <select className={Style.option}>
-                        <option value="Normal">Empréstimo Normal</option>
-                        <option value="Especial">Empréstimo Especial</option>
-                    </select>
+                <label>Tipo de empréstimo:</label>
+                <select className={Style.option}>
+                    <option value="Normal">Empréstimo Normal</option>
+                    <option value="Especial">Empréstimo Especial</option>
+                </select>
 
-                    <label>Categoria do usuário:</label>
-                    <select className={Style.option}>
-                        <option value="Aluno">Aluno</option>
-                        <option value="">Masculino</option>
-                        <option value="Outro">Outro</option>
-                    </select>
+                <br></br>
 
-                    <br />
+                <label>Categoria do usuário:</label>
+                <select className={Style.option}>
+                    <option value="Aluno">Aluno</option>
+                    <option value="Tercerizado">Tercerizado</option>
+                    <option value="Funcionário">Funcionário</option>
+                </select>
+
+                <br />
+                <div className={Style.inputContainer}>
+                    <input className={Style.inputButton} type="button" onClick={onButtonClick} value={'Registrar'} />
+                </div>
             </form>
 
         </>
