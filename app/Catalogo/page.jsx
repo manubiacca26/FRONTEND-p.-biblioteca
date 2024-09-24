65db4cd0d08d592661c19ce70748fd2e18dad23d
dudaribw <mr306573@gmail.com>
2024-09-24 09:06:29 -0300

    .

--- app/Catalogo/page.jsx
+++ app/Catalogo/page.jsx
@@ -4,3 +4,58 @@ import Styles from '@/app/Catalogo/catalogo.module.css'
 import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
 
+const Login = (props) => {
+    const [codigo, setEmail] = useState('')
+    const [autor, setPassword] = useState('')
+    const [titulo, setEmailError] = useState('')
+    const [assunto, setPasswordError] = useState('')
+    const [numero, setPasswordError] = useState('')
+    const [acervo, setPasswordError] = useState('')
+    const [ISBN, setPasswordError] = useState('')
+    const [quantidade, setPasswordError] = useState('')
+
+    return (
+        <>
+            <Header></Header>
+            <div className={Styles.formulario}></div>
+            <form>
+                <label>
+                    Código do exemplar:
+                    <input type="text" name="codigo" />
+                </label>
+                <label>
+                    Nome do autor:
+                    <input type="text" name="autor" />
+                </label>
+                <label>
+                    Título do exemplar:
+                    <input type="text" name="titulo" />
+                </label>
+                <label>
+                    Código do assunto:
+                    <input type="text" name="assunto" />
+                </label>
+                <label>
+                    Número de chamada:
+                    <input type="number" name="numero" />
+                </label>
+                <label>
+                    Acervo:
+                    <input type="text" name="acervo" />
+                </label>
+                <label>
+                    ISBN:
+                    <input type="text" name="ISBN" />
+                </label>
+                <label>
+                    Quantidade:
+                    <input type="number" name="quantidade" />
+                </label>
+                <div className={Styles.inputContainer}>
+                    <input className={Styles.inputButton} type="button" onClick={onButtonClick} value={'Registrar'} />
+                </div>
+            </form>
+            <Footer></Footer>
+        </>
+    )
+}
